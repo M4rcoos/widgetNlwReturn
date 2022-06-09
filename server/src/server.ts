2928ce546700aa5f561717dc03ec316df9fd35a7
Marcos <contato.shopvmax@gmail.com>
2022-06-09 10:41:31 -0300

    add port env variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,4 +7,4 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.listen(3333, () => console.log("server running!"));
+app.listen(process.env.PORT || 3333, () => console.log("server running!"));
